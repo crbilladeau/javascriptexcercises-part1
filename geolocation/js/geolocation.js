3e8dc6c3a65566764475faddfd13947da5467da3
charlie <charlie.billadeau@gmail.com>
2019-07-22 16:00:42 -0500

    Add files via upload

---
+++ geolocation/js/geolocation.js
@@ -0,0 +1,21 @@
+var elMap = document.getElementById('loc');
+var msg = 'Sorry, we were unable to get your location';
+
+if (Modernizr.geolocation) {
+  navigator.geolocation.getCurrentPosition(success, fail);
+  elMap.textContent = 'Checking location...';
+} else {
+  elMap.textContent = msg;
+}
+
+function success(position) {
+  msg = '<h3>Logitude:<br>';
+  msg += position.coords.longitude + '</h3>';
+  msg += '<h3>Latitude:<br';
+  msg += position.coords.latitude + '</h3>';
+  elMap.innerHTML = msg;
+}
+function fail(msg) {
+  elMap.textContent = msg;
+  console.log(msg.code);
+}
